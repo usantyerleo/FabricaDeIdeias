103bc120408bfccf37c946f5d845f8d104804e27
LUCAS LEONARDO MARTINS FARIAS <LUCAS7490@IOT>
2024-10-24 16:21:13 -0300

    com textura na home

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Footer from './components/Footer'
+import HomeTop from './pages/HomeTop'
+import HomeBottom from './pages/HomeBottom'
+
+function App() {
+
+
+  return (
+    <>
+
+      <HomeTop />
+
+      <HomeBottom />
+
+
+      <Footer />
+    </>
+  )
+}
+
+export default App
